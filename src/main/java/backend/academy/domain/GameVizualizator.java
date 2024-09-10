c2f0f6c19c5a4c29321d041b158e1f4980c33a46
plaffyyy <k.zimin@innopolis.university>
2024-09-10 22:14:41 +0300

    feat(databases): add databases for all categories of word

--- src/main/java/backend/academy/domain/GameVizualizator.java
+++ src/main/java/backend/academy/domain/GameVizualizator.java
@@ -1,5 +1,36 @@
 package backend.academy.domain;
 
-public class GameVisualizator {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class GameVizualizator {
+    private final static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private final static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+
+        writer.write("Здравствуй! Ты зашел в игру 'Виселица', " +
+            "укажи категорию слов, которая нравится больше всего из следующих:");
+        writer.flush();
+        writer.write("Математика   Медицина   Города мира   Штаты США   Страны");
+        writer.flush();
+        writer.write("Просто нажми 'Enter' для выбора случайной категории.");
+        writer.flush();
+        String category = reader.readLine();
+
+        writer.write("Теперь выбери уровень сложности из трех, " +
+            "число справа обозначает количество допустимых ошибок");
+        writer.flush();
+        writer.write("Легкий - 7 ошибок   Средний - 6 ошибок  Сложный - 5 ошибок");
+        writer.flush();
+        writer.write("Просто нажми 'Enter' для выбора случайного уровня сложности.");
+        writer.flush();
+        String level = reader.readLine();
+        writer.write(String.valueOf(level));
+        writer.flush();
+    }
+
 
 }
--- src/main/java/backend/academy/domain/WordsCategory.java
+++ src/main/java/backend/academy/domain/WordsCategory.java
@@ -1,5 +1,15 @@
 package backend.academy.domain;
 
-public interface WordsClass {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class WordsCategory {
+    private List<String> categories = new ArrayList<>(Arrays.asList("города мира", "страны", "математика", "штаты", "медицина"));
+    String category;
+    WordsCategory (String category) {
+        this.category = category;
+    }
+
 
 }
